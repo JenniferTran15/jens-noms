097d3520083ca389e3597c131aa9dbd09c4c8730
Jennifer <jennifertran135@gmail.com>
2019-06-18 02:06:48 +0000

    Geocoding address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode 
+  
   validates :name, length: { minimum: 2 } 
   validates :address, :description, presence: true
 end
